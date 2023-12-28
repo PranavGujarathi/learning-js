311379c66300ded2e3d187cb10a8e32057112989
PranavGujarathi <149895321+PranavGujarathi@users.noreply.github.com>
2023-12-28 10:06:05 +0000

    Data types

---
+++ 01_Basics/02DataTypes.js
@@ -0,0 +1,68 @@
+//weekly typed language
+
+//"use strict"; for using modern js
+
+//alert(5+5) for pop up notification
+
+// string==> ""
+console.log("Pranav");
+console.log('Pranav');
+//" " and '' both can be use 
+
+//Primitive and non primitive 
+
+// Primitive ==> 
+
+////weekly typed
+let num = 8;
+console.log(num + "1");
+console.log(typeof(num+"1"));        //string
+
+
+//Number
+let num1 = 10;
+const num2 = 3.14;
+console.log(typeof num1, typeof num2);        // number number  
+
+
+//String
+let str = "Pranav";
+const str2 = "Gujarathi";
+console.table([str,str2]);
+console.log(typeof str, typeof str2);         //string string   
+
+
+
+
+//Boolean==> haa ya na // true or false // 0 or 1
+let bool = true;
+const bool2 = false;
+console.log(typeof bool,typeof bool2);        //boolean boolean
+
+
+
+
+//Symbol==> uniqueness find
+const symbol = Symbol("age");                 // S
+console.log(typeof symbol);                   //symbol
+
+
+
+
+//Bigint
+let bigint = 1234567890123456789      // n in last
+console.log(typeof bigint);                   //bigint
+
+
+
+
+//Undefined==> declare but not assign values
+let Undefined; 
+console.log(typeof Undefined);                //undefined
+
+
+
+
+//Null==> Khali yafir Empty ==> assign value but empty 
+const number = null;
+console.log(typeof number);                   //object   ******remember
