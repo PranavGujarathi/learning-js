92df2e5fe8a2abdfcd6149a67310cdab94ded62e
PranavGujarathi <149895321+PranavGujarathi@users.noreply.github.com>
2023-12-28 10:15:08 +0000

    Basic

---
+++ 01_Basics/Basic.js
@@ -0,0 +1,13 @@
+let userName = "pranav";
+const password = "123456789";
+let email = "pranav@gmail.com";
+const phoneNumber = "8459235527";
+let isDisabled = false;
+let age = 20;
+let status = undefined;
+let height = null;
+
+console.log(`My Username is ${userName}`);
+console.log(`My password is ${password}`);
+console.log(`My email is ${email}`);
+console.log(`My phoneNumber is ${phoneNumber}`);
